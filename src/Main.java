f141150f87157d840bfccb54f8389fc6f354dcb5
Maxpower <maxpower2k@yandex.ru>
2022-03-06 09:57:46 +0300

    fix: изменить обработку # в комментариях

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,13 @@ public class Main {
         System.out.println("-----");
         String[] filenames = {"resources/shoppinglist.txt", "resources/minorder.txt"};
         for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
             if (args[i].equals("-list")) {
                 filenames[0] = args[i + 1];
-                System.out.println(filenames[0]);
             } else if (args[i].equals("-shops")) {
                 filenames[1] = args[i + 1];
-                System.out.println(filenames[1]);
             }
             i += 1;
         }
-        System.out.println(filenames[0] + " " + filenames[1]);
         ChoiceMarketManager choiceMarketManager = new ChoiceMarketManager();
         choiceMarketManager.run(filenames);
     }
